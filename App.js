06023b160d4793f979a1c0966971f2f58f50941b
MarcelloSA <103936996+msakal@users.noreply.github.com>
2022-11-08 09:57:54 -0300

    criando área de scroll no componente provacidade

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import Favoritos from "./src/screens/Favoritos";
 import FormBusca from "./src/screens/FormBusca";
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
+import Privacidade from "./src/screens/Privacidade";
 
 const App = () => {
   return (
     <>
       {/* opções para o barStyle: dark-content, light-content ou default */}
       <StatusBar barStyle="default" />
-      <FormBusca />
+      {/*  <FormBusca />
       <Favoritos />
-      <Sobre />
+      <Sobre /> */}
+      <Privacidade />
     </>
   );
 };
