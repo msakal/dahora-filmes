873fffc32b7437622c2cd03d505d12d8ac5fffd9
MarcelloSA <103936996+msakal@users.noreply.github.com>
2022-11-09 11:30:34 -0300

    concluindo exercício
    dando ação ao formulário de busca

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,13 +1,84 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Alert,
+  Button,
+  TextInput,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+
+const corPrimaria = "#5451a6";
 
 const FormBusca = () => {
+  /* state para o filme que será buscado */
+  const [filme, setFilme] = useState("");
+
+  /* evento de captura do texinput a partir do onchangetext (em tempo real). */
+  const capturaDigitacao = (valorDigitado) => {
+    setFilme(valorDigitado);
+  };
+
+  const inputTexto = () => {
+    if (!filme) {
+      Alert.alert("Ops!", "Você deve digitar o nome de um filme"),
+        [{ filme: "OK" }];
+      return;
+    }
+    Alert.alert("Você procurou por:", filme), [{ filme: "OK" }];
+  };
+
   return (
-    <SafeAreaView>
-      <Text>FormBusca</Text>
+    <SafeAreaView style={estilos.container}>
+      <Text style={estilos.titulo}>
+        Star Trek? O Poderoso Chefão? A trilogia Senhor dos Anéis?
+      </Text>
+
+      <Text style={estilos.titulo}>
+        Localize um filme que você viu ou gostaria de ver!
+      </Text>
+
+      <View style={estilos.caixaInput}>
+        <Ionicons name="film" size={44} color="black" />
+        <TextInput
+          style={estilos.inPut}
+          onChangeText={capturaDigitacao}
+          placeholder="Filme..."
+        />
+      </View>
+
+      <Button title="Buscar" color={corPrimaria} onPress={inputTexto} />
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    /*  marginHorizontal: 16,
+    justifyContent: "center", */
+  },
+  titulo: {
+    textAlign: "left",
+    marginVertical: 8,
+  },
+  inPut: {
+    flex: 1,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  caixaInput: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  botao: {
+    backgroundColor: corPrimaria,
+  },
+});
